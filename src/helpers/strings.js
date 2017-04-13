1d7ac108a82490fd3a9c21c3b7547871a6f13d12
Anas Mazouni <madadj4@gmail.com>
2017-04-13 23:29:04 +0100

    Custom Strings.js

--- src/helpers/strings.js
+++ src/helpers/strings.js
@@ -1,12 +1,30 @@
 module.exports = {
   queryString: [
-    ''
+    'google',
+    '100daysofcode',
+    'freecodecamp',
+    'github',
+    'gitlab',
+    'atom.io',
+    'nodejs',
+    'node.js',
+    'php.net',
+    'laravelnews',
+    'Laravel News',
+    'GitLab',
+    'javascript',
+    'web dev'
   ],
   queryStringSubQuery: [
-    ''
+    '',
+    ' best practices',
+    ' handy tip',
+    ' how to',
+    ' resources'
   ],
   blockedStrings: [
-    ''
+    'porn',
+    'sex'
   ],
   resultType: [
     'mixed',
