3aeb60cb68837f35d20e631d878140ee3b8f94cc
Anas Mazouni <madadj4@gmail.com>
2017-04-13 23:43:13 +0100

    ....

--- src/helpers/strings.js
+++ src/helpers/strings.js
@@ -1,37 +1,39 @@
 module.exports = {
-  queryString: [
-    'google',
-    '100daysofcode',
-    'freecodecamp',
-    'github',
-    'gitlab',
-    'atom.io',
-    'nodejs',
-    'node.js',
-    'php.net',
-    'laravelnews',
-    'Laravel News',
-    'GitLab',
-    'javascript',
-    'web dev'
-  ],
-  queryStringSubQuery: [
-    '',
-    ' best practices',
-    ' handy tip',
-    ' how to',
-    ' resources'
-  ],
-  blockedStrings: [
-    'porn',
-    'sex'
-  ],
-  resultType: [
-    'mixed',
-    'recent',
-    'popular'
-  ],
-  responseString: [
-    'Hi @screenName thanks for the following!'
-  ]
+    queryString: [
+        'google',
+        '100daysofcode',
+        'freecodecamp',
+        'github',
+        'gitlab',
+        'atom.io',
+        'nodejs',
+        'node.js',
+        'php.net',
+        'laravelnews',
+        'Laravel News',
+        'GitLab',
+        'javascript',
+        'web dev'
+    ],
+    queryStringSubQuery: [
+        '',
+        ' best practices',
+        ' handy tip',
+        ' how to',
+        ' resources'
+    ],
+    blockedStrings: [
+        'porn',
+        'sex'
+    ],
+    resultType: [
+        'mixed',
+        'recent',
+        'popular'
+    ],
+    responseString: [
+        'Hi @screenName thanks for the following!',
+        '@screenName thanks for following :D',
+        'Hey @screenName thanks for the follow'
+    ]
 }
